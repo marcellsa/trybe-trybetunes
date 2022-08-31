a0d08278d66f2a133d9e69251df7780f72b5bc88
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-08-31 16:38:05 -0300

    Formaulário para identificação criado.

---
+++ src/pages/LoadingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoadingPage extends Component {
+  render() {
+    return (
+      <h1>
+        Carregando...
+      </h1>
+    );
+  }
+}
+
+export default LoadingPage;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import LoadingPage from './LoadingPage';
+
+const MIN_USERNAME_LENGTH = 3;
 
 class Login extends Component {
+  state = {
+    userName: '',
+    loggedIn: false,
+  };
+
+  handleUserNameChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      userName: value,
+    });
+  };
+
+  handleButton = () => {
+    this.setState({
+      loggedIn: true,
+    }, async () => {
+      const { userName } = this.state;
+      await createUser({ name: userName });
+      const { history } = this.props;
+      history.push('/search');
+    });
+  };
+
   render() {
+    const { userName, loggedIn } = this.state;
+
     return (
       <div data-testid="page-login">
-        Login
+        { loggedIn ? (<LoadingPage />)
+          : (
+            <form>
+              <label htmlFor="login-name-input">
+                Login
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  name="login-name-input"
+                  id="login-name-input"
+                  value={ userName }
+                  onChange={ this.handleUserNameChange }
+                />
+              </label>
+
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ userName.length < MIN_USERNAME_LENGTH }
+                onClick={ this.handleButton }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
