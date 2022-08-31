c8bcc13ef94f25b2c3d813cb5a2b4305c814bb0e
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-08-31 19:16:26 -0300

    Links de navegação no cabeçalho criados.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import LoadingPage from '../pages/LoadingPage';
 
@@ -29,6 +29,11 @@ class Header extends Component {
         <h2 data-testid="header-user-name">
           { response ? <LoadingPage /> : user}
         </h2>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites </Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile </Link>
+        </nav>
       </header>
     );
   }
