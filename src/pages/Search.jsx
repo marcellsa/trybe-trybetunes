989313f4f986eae17c8440306f5372183faffc0c
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-09-01 11:28:48 -0300

    Formulário para pesquisa de artistas criado.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const MIN_ARTISTNAME_LENGTH = 2;
+
 class Search extends Component {
+  state = {
+    artistName: '',
+  };
+
+  handleArtistInput = (event) => {
+    const { value } = event.target;
+    this.setState({
+      artistName: value,
+    });
+  };
+
   render() {
+    const { artistName } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+
+        <form>
+          <label htmlFor="search-artist-input">
+
+            <input
+              data-testid="search-artist-input"
+              t
+              ype="text"
+              name="search-artist-input"
+              id="search-artist-input"
+              placeholder="Nome do Artista"
+              value={ artistName }
+              onChange={ this.handleArtistInput }
+            />
+
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ artistName.length < MIN_ARTISTNAME_LENGTH }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
